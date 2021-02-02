9bae75179614654b5cf846ae7eadd5141ddc6299
bsummerset <brian.summerset@gmail.com>
2021-02-01 23:55:18 -0500

    react project

---
+++ src/pages/About.js
@@ -0,0 +1,13 @@
+
+import React from "react";
+
+export default function About() {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+       When the light is lit, so should you!!!... This application was created to help you create the perfect cocktail. You now have the ability to search your cocktail of choice and are given not only the ingredients for the cocktail you fancy, but also the instructions on how to prepare the proper cocktail glass for the perfect beverage experience. Now it's time to have the perfect cocktail anywhere you desire. 
+      </p>
+    </section>
+  );
+}
